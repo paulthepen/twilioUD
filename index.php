1a904decfb130c6f63964a01b5b8ad43b27472aa
Paul Charpie <paulcharpie@gmail.com>
2021-08-05 02:43:14 +0000

    removed this

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 require __DIR__ . '/vendor/autoload.php';
 
 $db = mysqli_connect('localhost', 'twilio_app', '5H!afoNHxD${LJ9#', 'twilio_ud');
-$query = mysqli_query($this->db, 'SELECT * FROM call_sms');
-$results = $this->query->fetch_array(MYSQLI_ASSOC);
+$query = mysqli_query($db, 'SELECT * FROM call_sms');
+$results = $query->fetch_array(MYSQLI_ASSOC);
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
