f47d80d74f5f1e75735f7769b074fe573d2a33b5
Paul Charpie <paulcharpie@gmail.com>
2021-08-05 02:35:18 +0000

    corrected var declarations

--- src/App.php
+++ src/App.php
@@ -15,12 +15,12 @@ class App
    * @var \Slim\App
    */
     private $app;
+    private $db = mysqli_connect('localhost', 'twilio_app', '5H!afoNHxD${LJ9#', 'twilio_ud');
+    private $query = mysqli_query($this->db, 'SELECT * FROM call_sms');
+    private $results = $this->query->fetch_array(MYSQLI_ASSOC);
+
 
     public function __construct() {
-        $db = mysqli_connect('localhost', 'twilio_app', '5H!afoNHxD${LJ9#', 'twilio_ud');
-        $query = mysqli_query($db, 'SELECT * FROM call_sms');
-        $results = $query->fetch_array(MYSQLI_ASSOC);
-
         $app = AppFactory::create();
 
         $app->post(
