d9bf9841f8383e9c961d6bd5b3f9cdf16a127137
Paul Charpie <paulcharpie@gmail.com>
2021-08-06 17:12:06 +0000

    style changes

--- src/dashboard.php
+++ src/dashboard.php
@@ -16,24 +16,26 @@ $query = mysqli_query($db, 'SELECT * FROM call_sms');
     </head>
     <body>
         <div class="container">
-        <h1>UD Automated SMS</h1>
-        <?php 
-            while ($configuration = $query->fetch_assoc()) { ?>
-                <div class='col-xs-12'>
-                    <h4><?php echo $configuration['name']; ?></h4>
-                </div>
-                <div class='col-xs-12 form-group'>
-                    <div class='col-xs-4'>
-                    <label class='control-label'>Twilio Number</label><input class="form-control" type="text" name="number-<?php echo $configuration['id']; ?>" value="<?php echo $configuration['twilio_number']; ?>" />
-                    </div>
-                    <div class='col-xs-4'>
-                    <label class='control-label'>Audio Link</label><input class="form-control" type="text" name="audio-<?php echo $configuration['id']; ?>" value="<?php echo $configuration['audio_link']; ?>" />
-                    </div>
-                    <div class='col-xs-4'>
-                    <label class='control-label'>Message To Text</label><input class="form-control" type="text" name="message-<?php echo $configuration['id']; ?>" value="<?php echo $configuration['message_text']; ?>" />
-                    </div>
-                </div>
-        <?php } ?>
+            <div class="col-sm-8 col-sm-offset-2" id="content">
+                <h1>UD Automated SMS</h1>
+                <?php 
+                    while ($configuration = $query->fetch_assoc()) { ?>
+                        <div class='col-xs-12'>
+                            <h4><?php echo $configuration['name']; ?></h4>
+                        </div>
+                        <div class='col-xs-12 form-group'>
+                            <div class='col-xs-4'>
+                            <label class='control-label'>Twilio Number</label><input class="form-control" type="text" name="number-<?php echo $configuration['id']; ?>" value="<?php echo $configuration['twilio_number']; ?>" />
+                            </div>
+                            <div class='col-xs-4'>
+                            <label class='control-label'>Audio Link</label><input class="form-control" type="text" name="audio-<?php echo $configuration['id']; ?>" value="<?php echo $configuration['audio_link']; ?>" />
+                            </div>
+                            <div class='col-xs-4'>
+                            <label class='control-label'>Message To Text</label><input class="form-control" type="text" name="message-<?php echo $configuration['id']; ?>" value="<?php echo $configuration['message_text']; ?>" />
+                            </div>
+                        </div>
+                <?php } ?>
+            </div>
         </div>
     </body>
 </html>
