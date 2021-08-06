8a9aaa51dd976d6ee3560748833d914b30db06db
Paul Charpie <paulcharpie@gmail.com>
2021-08-06 17:34:18 +0000

    bootstrap flex

--- src/dashboard.php
+++ src/dashboard.php
@@ -23,18 +23,18 @@ $query = mysqli_query($db, 'SELECT * FROM call_sms');
                         <div class='col-xs-12'>
                             <h4><?php echo $configuration['name']; ?></h4>
                         </div>
-                        <div class='col-xs-12 form-group' style="padding: 10px; background-color: #aaa; border: 1px solid black; border-radius: 5px;">
-                            <div class='col-xs-4'>
+                        <div class='d-flex form-group' style="padding: 10px; background-color: #aaa; border: 1px solid black; border-radius: 5px;">
+                            <div>
                             <label class='control-label'>Twilio Number</label><input class="form-control" type="text" name="number-<?php echo $configuration['id']; ?>" value="<?php echo $configuration['twilio_number']; ?>" />
                             </div>
-                            <div class='col-xs-4'>
+                            <div>
                             <label class='control-label'>Audio Link</label><input class="form-control" type="text" name="audio-<?php echo $configuration['id']; ?>" value="<?php echo $configuration['audio_link']; ?>" />
                             </div>
-                            <div class='col-xs-4'>
+                            <div>
                             <label class='control-label'>Message To Text</label><input class="form-control" type="text" name="message-<?php echo $configuration['id']; ?>" value="<?php echo $configuration['message_text']; ?>" />
-                        </div>
-                        <div class="pull-right">\
-                            <button class="btn btn-primary"><i class="fa fa-save"></i></button>
+                            <div class="flex-shrink">
+                                <button class="btn btn-primary"><i class="fa fa-save"></i></button>
+                            </div>
                         </div>
                 <?php } ?>
                 <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addForm" style="position: fixed; bottom: 25px; right: 75px; "><i class="fa fa-3x fa-plus-circle"></i></button>
