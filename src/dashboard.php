1e97763f9c19c0fff6ac6ed02be50db4a3446504
Paul Charpie <paulcharpie@gmail.com>
2021-08-06 17:09:15 +0000

    bootstrap mods

--- src/dashboard.php
+++ src/dashboard.php
@@ -12,24 +12,24 @@ $query = mysqli_query($db, 'SELECT * FROM call_sms');
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="custom-style.css">
+        <link rel="stylesheet" href="./custom-style.css">
     </head>
     <body>
         <div class="container">
         <h1>UD Automated SMS</h1>
         <?php 
             while ($configuration = $query->fetch_assoc()) { ?>
-                <div class='col-12'>
+                <div class='col-xs-12'>
                     <h4><?php echo $configuration['name']; ?></h4>
                 </div>
-                <div class='col-12 form-group'>
-                    <div class='col-4'>
+                <div class='col-xs-12 form-group'>
+                    <div class='col-xs-4'>
                     <label class='control-label'>Twilio Number</label><input class="form-control" type="text" name="number-<?php echo $configuration['id']; ?>" value="<?php echo $configuration['twilio_number']; ?>" />
                     </div>
-                    <div class='col-4'>
+                    <div class='col-xs-4'>
                     <label class='control-label'>Audio Link</label><input class="form-control" type="text" name="audio-<?php echo $configuration['id']; ?>" value="<?php echo $configuration['audio_link']; ?>" />
                     </div>
-                    <div class='col-4'>
+                    <div class='col-xs-4'>
                     <label class='control-label'>Message To Text</label><input class="form-control" type="text" name="message-<?php echo $configuration['id']; ?>" value="<?php echo $configuration['message_text']; ?>" />
                     </div>
                 </div>
